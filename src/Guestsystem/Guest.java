706a0d93156e00363670275a1bf24820a763954d
hanaSameh267 <hanasamehnour7@gmail.com>
2026-04-10 16:36:36 +0200

    add guest class implementation.

--- src/Guestsystem/Guest.java
+++ src/Guestsystem/Guest.java
@@ -35,7 +35,7 @@ public class Guest {
                 return g;
             }
         }
-        System.out.println("Login failed, please try again.");
+        System.out.println("Login failed.");
         return null;
     }
     public void viewAvailableRooms(){                         //person 3
