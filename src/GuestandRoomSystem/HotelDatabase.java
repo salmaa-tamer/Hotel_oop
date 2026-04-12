83dbea26351c21b128f5b72ad516cee4920611d3
Mohamed <mohamednasser12018@gmail.com>
2026-04-12 17:32:20 +0200

    created database file and assigned the arrays

---
+++ src/GuestandRoomSystem/HotelDatabase.java
@@ -0,0 +1,15 @@
+package GuestandRoomSystem;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HotelDatabase {
+    // 1. The static ArrayLists connecting everything
+    public static ArrayList<Guest> guests = new ArrayList<>();
+    public static ArrayList<Room> rooms = new ArrayList<>();
+    public static ArrayList<Reservation> reservations = new ArrayList<>();
+    public static ArrayList<Bill> bills = new ArrayList<>();
+
+
+}
