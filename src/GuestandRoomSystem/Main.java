bb8d9d711dc0f7738b661ef1e01f9c149eff567f
Sara <saramoh20107@gmail.com>
2026-04-15 14:48:41 +0200

    main

--- src/GuestandRoomSystem/Main.java
+++ src/GuestandRoomSystem/Main.java
@@ -5,12 +5,24 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public  static void main(String []args){
+
         HotelDatabase.loadDummyData();
         System.out.println("Enter Username : ");
-        String name = scanner.nextLine();
+        String username = scanner.nextLine();
         System.out.println("Enter Password");
         String password = scanner.nextLine();
-        System.out.println("Enter Date of Birth");
+        Guest guest = Guest.login(username ,password);
+        guest = HotelDatabase.guests.get(0);
+        guest.register();
+        guest.viewAvailableRooms();
+        System.out.println("Guest makes reservation test");
+        System.out.println("Enter check in date");
+        String checkinDateInput = scanner.nextLine();
+        LocalDate checkinDate = LocalDate.parse(checkinDateInput);
+        System.out.println("Enter check out date");
+        String checkoutDateInput = scanner.nextLine();
+        LocalDate checkoutDate = LocalDate.parse(checkoutDateInput);
+        guest.makeReservation(room.getroomid,checkinDate,checkoutDate);
 
 
     }
