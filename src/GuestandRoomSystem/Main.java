fe72a5ac79bb4254797ba6ab142fb276bface492
Sara <saramoh20107@gmail.com>
2026-04-15 13:08:25 +0200

    main addition

--- src/GuestandRoomSystem/Main.java
+++ src/GuestandRoomSystem/Main.java
@@ -1,10 +1,16 @@
 package GuestandRoomSystem;
 
+import java.time.LocalDate;
 import java.util.Scanner;
-
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public  static void main(String []args){
+        HotelDatabase.loadDummyData();
+        System.out.println("Enter Username : ");
+        String name = scanner.nextLine();
+        System.out.println("Enter Password");
+        String password = scanner.nextLine();
+        System.out.println("Enter Date of Birth");
 
 
     }
