bdc22ecabd4ebe5fc07f1116092bd91dbd5b09fc
Mohamed <mohamednasser12018@gmail.com>
2026-04-18 21:53:13 +0200

    removed unused interfaces

--- src/GuestandRoomSystem/Payable.java
+++
@@ -1,5 +0,0 @@
-package GuestandRoomSystem;
-
-public interface Payable {
-    void pay(double amount, PaymentMethod method);
-}
--- src/StaffSystem/Manageable.java
+++
@@ -1,7 +0,0 @@
-package StaffSystem;
-
-public interface Manageable {
-    void add(Object item);
-    void remove(Object item);
-    void update(Object item);
-}
