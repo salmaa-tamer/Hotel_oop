044f71a90fda46ef26992f2f321e2da8e0c0bbf6
Reem Hetta <reemhetta@gmail.com>
2026-04-25 21:29:50 +0300

    Added AdminGUI Testing Main

---
+++ src/GUI/CODE/AdminTestMain.java
@@ -0,0 +1,28 @@
+package GUI.CODE;
+
+import GuestandRoomSystem.HotelDatabase;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminTestMain extends Application {
+
+        @Override
+        public void start(Stage stage) throws Exception {
+
+            HotelDatabase.loadDummyData();
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/GUI/FXML/AdminDashboard.fxml")
+            );
+            Scene scene=new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        }
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+}
